1cd80bb1baf9eb5855466439e2adcc8e73917c83
Josh Bridges <48564669+Jbridg12@users.noreply.github.com>
2021-10-14 13:45:34 -0400

    Add files via upload

---
+++ myProgram.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+
+
+N = sys.argv[1]
+ptaken = sys.argv[2]
+pmask = sys.argv[3]
+psick_empty = sys.argv[4]
+psick_masked = sys.argv[5]
+psick_nomask = sys.argv[6]
+rsick = sys.argv[7]
+rexit = sys.argv[8]
+rlearn = sys.argv[9]
+
+
+def CalcValue(state, action, valuefunc):
+    return 1
+    
+def 
+
