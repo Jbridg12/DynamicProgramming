6ba8da4b38d2e58ac148bfe6775bbe9c00c1a38d
Jbridg12 <48564669+Jbridg12@users.noreply.github.com>
2021-10-14 16:23:07 -0400

    start

--- myProgram.py
+++ myProgram.py
@@ -12,9 +12,31 @@ rsick = sys.argv[7]
 rexit = sys.argv[8]
 rlearn = sys.argv[9]
 
+states = [3][N]
 
-def CalcValue(state, action, valuefunc):
+def State():
+    def __init__(self, taken, mask, index):
+        self.taken = taken
+        self.mask = mask
+        self.index = index
+        self.value = 0
+
+def CalcValue(state, action):
+    if action == "move":
+        if state.index == N-1:
+            nt = (1-ptaken)
+        else:    
+            nt = (1-ptaken)(0+
+    if state.taken:
+        if state.mask:
+            
+        else:
+    else:
+    res = valuefunc[]
     return 1
     
-def 
+def ValueIteration():
+    return 1
 
+def PolicyImprov(valuefunc):
+    return 1
