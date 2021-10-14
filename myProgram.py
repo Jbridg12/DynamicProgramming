5f8cd1a76a733242a81a57953188c1c8fb2f65b2
Jbridg12 <48564669+Jbridg12@users.noreply.github.com>
2021-10-14 17:40:11 -0400

    phase 2

--- myProgram.py
+++ myProgram.py
@@ -1,42 +1,67 @@
 import sys
 import numpy as np
 
-
-N = sys.argv[1]
-ptaken = sys.argv[2]
-pmask = sys.argv[3]
-psick_empty = sys.argv[4]
-psick_masked = sys.argv[5]
-psick_nomask = sys.argv[6]
-rsick = sys.argv[7]
-rexit = sys.argv[8]
-rlearn = sys.argv[9]
-
-states = [3][N]
-
-def State():
+gamma = 0.9
+threshold = 0.0001
+class State:
     def __init__(self, taken, mask, index):
         self.taken = taken
         self.mask = mask
         self.index = index
-        self.value = 0
+        self.value = 0.0
+        self.policy = "move"
+        
+        
+N = int(sys.argv[1])
+ptaken = float(sys.argv[2])
+pmask = float(sys.argv[3])
+psick_empty = float(sys.argv[4])
+psick_masked = float(sys.argv[5])
+psick_nomask = float(sys.argv[6])
+rsick = float(sys.argv[7])
+rexit = float(sys.argv[8])
+rlearn = float(sys.argv[9])
+
+
+states = []
+for i in range(1, N+1):
+    states.insert(0, [State(False, True, i), State(True, True, i), State(True, False, i)])
+
 
 def CalcValue(state, action):
     if action == "move":
-        if state.index == N-1:
-            nt = (1-ptaken)
-        else:    
-            nt = (1-ptaken)(0+
-    if state.taken:
-        if state.mask:
-            
+        if state.index == 1:
+            return rexit
         else:
-    else:
-    res = valuefunc[]
-    return 1
+            nt = (1-ptaken)*(gamma*states[state.index-1][0].value)
+            tm = ((ptaken*pmask)/ptaken)*(gamma*states[state.index-1][1].value)
+            tnm = ((ptaken*(1-pmask))/ptaken)*(gamma*states[state.index-1][2].value)
+            return nt+tm+tnm        
+    elif action == "sit":
+        if state.taken:
+            if state.mask:
+                return (psick_masked * rsick) + ((1-psick_masked) * (rlearn*(N-state.index)))
+            else:
+                return (psick_nomask * rsick) + ((1-psick_nomask) * (rlearn*(N-state.index)))
+        else:
+            return (psick_empty * rsick) + ((1-psick_empty) * (rlearn*(N-state.index)))
+    return 
     
+
+   
 def ValueIteration():
-    return 1
+    while True:
+        delta = 0
+        for ele in states:
+            for s in ele:
+                v = s.value
+                s.value = min(CalcValue(s, "move"), CalcValue(s, "stay")) 
+                error = abs(v-s.value)
+                if(error > delta):
+                    delta = error
+        
+        if delta >= threshold:
+            break
 
 def PolicyImprov(valuefunc):
     return 1
